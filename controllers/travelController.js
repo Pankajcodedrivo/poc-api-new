dcf377c5b08674871548c9c2cd47868a97212bcf
Bittu Shaw <bshaw891021@gmail.com>
2025-10-11 16:59:09 +0530

    changes

--- controllers/travelController.js
+++ controllers/travelController.js
@@ -27,7 +27,7 @@ const sendFeedbackForm = async (req, res) => {
       .join('');
 
     await sendEmail({
-      to: process.env.SENDGRID_FROM_EMAIL, // your receiving email
+      to: `${process.env.SENDGRID_FROM_EMAIL},bittus@scaleupsoftware.io`, // your receiving email
       subject: "New Feedback Received",
       html: `<h2>New Feedback Submission</h2>${emailContent}`
     });
