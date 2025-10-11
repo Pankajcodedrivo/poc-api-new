5ea0ce83d860e7ed3405a6b9a169afafa5d708ac
Bittu Shaw <bshaw891021@gmail.com>
2025-10-11 17:06:03 +0530

    changes

--- routes/travelRoutes.js
+++ routes/travelRoutes.js
@@ -5,7 +5,7 @@ const travelValidator = require('../validators/travelValidator');
 const router = express.Router();
 
 router.post('/', travelValidator.validateTravelInput, travelController.createTravelPlan);
-router.post('/feedback', travelValidator.validateFeedbackInput, travelController.sendFeedbackForm);
+router.post('/feedback', travelController.sendFeedbackForm);
 router.post('/sendEmail',  travelController.sendTravelPlanEmail);
 
 module.exports = router;
